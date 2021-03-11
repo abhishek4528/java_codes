7e55534e5a57020d09212fc5ab5772d20224c03b
Abhishek <80439876+abhishek4528@users.noreply.github.com>
2021-03-11 20:17:24 +0530

    Update Season_activity_informer.java

--- Season_activity_informer.java
+++ Season_activity_informer.java
@@ -7,10 +7,12 @@ public class Season_activity_informer {
 		Scanner s = new Scanner(System.in);
 	int n= s.nextInt();
 	String res;
+		
 	res = (n==3||n==4||n==5) ? "Spring-Season(Vasant Ritu)\n"   : 
-          (n==6||n==7||n==8)?  "Summer-Season(Grishma Ritu)\n"  : 
+          (n==6||n==7||n==8)?  "Summer-Season(Grishma Ritu)\n"  :      //using ternary operator.
           (n==9||n==10||n==11)?"Autumn Season(Sharad Ritu)\n"   :
           (n==12||n==1||n==2)? "Winter Season(Shishir Ritu)\n"  : "No result\n";
+		
     String decision = s.nextLine();
     
 	System.out.print(res);
